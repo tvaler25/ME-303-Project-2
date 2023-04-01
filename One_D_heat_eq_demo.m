16f5ba8674af65eb9ed63d3df538a8e52e433135
tvaler25 <129217422+tvaler25@users.noreply.github.com>
2023-04-01 16:50:53 -0400

    Update One_D_heat_eq_demo.m

--- One_D_heat_eq_demo.m
+++ One_D_heat_eq_demo.m
@@ -4,12 +4,14 @@ clc
 close all
 
 L= 1;         % x in (0,L)
-T= 0.5;       % t in (0,T)
-k=10;    % conductivity
+T= 0.1;       % t in (0,T)
+k=2;    % conductivity
 N=20;   % cut space into N sections
-M=5000; % cut time  into M sections
+M=2500; % cut time  into M sections
 dx=L/N; dt=T/M; % grid spacing
+
 F=k*dt/dx^2;
+
 
 temp = zeros(N+1, M+1);
 
@@ -17,7 +19,7 @@ temp = zeros(N+1, M+1);
 x = linspace(0, L, N+1);
  
 % Initial Condition
-temp(:, 1) = sin(pi * x);
+temp(:, 1) = cos(pi * x);
  
 % Explicit Scheme for Partial Difference Equation
 for j=1:M % time coordinate = j/M
@@ -29,41 +31,10 @@ for j=1:M % time coordinate = j/M
 %     temp(1, j+1) = 2 + temp(2, j+1); % NBC left
     
 %     temp(N+1, j+1) = 0; % DBC right: a constant BC
-    temp(N+1, j+1) = sin(pi * (j+1)/M); % DBC right: a time-varying one
+    temp(N+1, j+1) = 2; % DBC right: a time-varying one
 end
  
 
-%% animation
-h = figure();
-ax = gca;
-ax.NextPlot = 'replaceChildren';
-
-every_nth_frame = 50; %index freqeuncy for animation
-num_frames = floor(M/every_nth_frame); %total number of frames
-
-ani_temp = temp(:, 1:every_nth_frame:end); %temperature sampled at specified frequency
-ax.XLim = [0, L];
-ax.YLim = [min(ani_temp, [], "all"), max(ani_temp, [], 'all')];
-xlabel('x'); ylabel('T(x,t)');
-
-for idx = 1:num_frames
-    plot(x, ani_temp(:, idx), '-r')
-    drawnow
-    frame = getframe(h);
-    im{idx} = frame2im(frame);
-end
-close;
-
-%save the gif, uncomment to save
-% filename = "testAnimated.gif"; % Specify the output file name
-% for idx = 1:num_frames
-%     [A,map] = rgb2ind(im{idx},256);
-%     if idx == 1
-%         imwrite(A,map,filename,"gif","LoopCount",Inf,"DelayTime",0);
-%     else
-%         imwrite(A,map,filename,"gif","WriteMode","append","DelayTime",0);
-%     end
-% end
 
 
 %% plot
@@ -72,6 +43,8 @@ figure()
 surf(T, X, temp); 
 shading interp
 colormap('hot')
-xlabel('t'); ylabel('x'); zlabel('T(x,t)'); colorbar
+xlabel('t'); 
+ylabel('x'); 
+zlabel('T(x,t)'); colorbar
 
 
