82a2b7f35ac529bc50d84cac9b7e39863dbdc597
tvaler25 <129217422+tvaler25@users.noreply.github.com>
2023-04-03 18:30:49 -0400

    Project 2
    adding part a comparison

--- Question2.m
+++ Question2.m
@@ -3,88 +3,120 @@ clear
 clc
 close all
 
-grid_spacing=[0.001,0.01,0.1,10];
-for dx=1:1:4 %loop through each gridspacing 
-    dx=grid_spacing(1);
-    dt=grid_spacing(1);
-end % still need to figure out how to compare
-
 L= 1;         % x in (0,L)
-T= 0.5;       % t in (0,T)
+T= 10;       % t in (0,T)
 k=2;    % conductivity
-N=20;   % cut space into N sections
 
+%grid spacing
+%dt=0.03;
 
-M=2500; % cut time  into M sections
+N=50;   % cut space into N sections
+M=150000; % cut time  into M sections
 J=1000; % use J iterations of the summation
-dx=L/N; dt=T/M; % grid spacing
+dx=L/N; 
+dt=T/M; % grid spacing
 
 F=k*dt/dx^2;
 
-%Find Cn from n=1 to n=J
-Cn = zeros(J, 1);
-Cn(1) = -4; %using limits since term evaluates to 0/0
-for n=2:1:J
-    Cn(n) = 2*((n^2)-((-1)^n)*(2-(3*(n^2))))/(n*((n^2)-1));
-end
-Cn = Cn/pi;
+if (1-2*F)>0
 
-temp = zeros(N+1, M+1);
-exact = zeros(N+1, M+1);
-
-% Position of nodes
-x = linspace(0, L, N+1);
- 
-% Initial Condition
-temp(:, 1) = cos(pi * x);
-temp(1,1)=0;
-temp(N+1,1)=2;
- 
-% Explicit Scheme for Partial Difference Equation
-for j=1:M % time coordinate = j/M
-    
-    for i=2:N % space coordinate = i/N
-        temp(i, j+1) = temp(i, j) + F * (temp(i+1, j) - 2*temp(i, j) + temp(i-1, j));
-        
-        
+    %Find Cn from n=1 to n=J
+    Cn = zeros(J, 1);
+    Cn(1) = -4; %using limits since term evaluates to 0/0
+    for n=2:1:J
+        Cn(n) = 2*((n^2)-((-1)^n)*(2-(3*(n^2))))/(n*((n^2)-1));
     end
-    temp(1, j+1) = 0; % DBC left
+    Cn = Cn/pi;
     
-    temp(N+1, j+1) = 2; % DBC right: a time-varying one
-end
-
-for j=1:M+1
-
-    for i=1:N+1
-        %find exact temperature
-        exact(i, j) = exactTemp(((i-1)*dx), ((j-1)*dt), Cn);
+    temp = zeros(N+1, M+1);
+    exact = zeros(N+1, M+1);
+    
+    % Position of nodes
+    x = linspace(0, L, N+1);
+     
+    % Initial Condition
+    temp(:, 1) = cos(pi * x);
+    temp(1,1)=0;
+    temp(N+1,1)=2;
+     
+    % Explicit Scheme for Partial Difference Equation
+    for j=1:M % time coordinate = j/M
+        
+        for i=2:N % space coordinate = i/N
+            temp(i, j+1) = temp(i, j) + F * (temp(i+1, j) - 2*temp(i, j) + temp(i-1, j));
+            
+            
+        end
+        temp(1, j+1) = 0; % DBC left
+        
+        temp(N+1, j+1) = 2; % DBC right: a time-varying one
     end
+    
+    for j=1:M+1
+    
+        for i=1:N+1
+            %find exact temperature
+            exact(i, j) = exactTemp(((i-1)*dx), ((j-1)*dt), Cn);
+        end
+    end
+    
+    
+    %% plot
+    
+    figure('Name','Analytical')
+    [X, Time] = meshgrid(0:dt:T, x);
+    surf(X,Time, exact)
+    shading interp
+    colormap('jet')
+    xlabel('t'); 
+    ylabel('x'); 
+    zlabel('T(x,t)'); colorbar
+    
+    figure(2)
+    [Time, X] = meshgrid(0:dt:T, x); 
+    surf(Time, X, temp); 
+    shading interp
+    colormap('jet')
+    xlabel('t'); 
+    ylabel('x'); 
+    zlabel('T(x,t)'); colorbar
+    
+    
+   
+    sliced_time_indices=round([0.001,0.01,0.1,10]/dt);
+    num_slices=length(sliced_time_indices);
+    
+    slices=zeros(N+1,num_slices*2);
+    
+    for slice_number=1:num_slices %creating slices to compare profiles of ndiffernt time values
+        slices(:,slice_number)=temp(:, sliced_time_indices(slice_number));
+        slices(:,slice_number+4)=exact(:, sliced_time_indices(slice_number));
+    end
+
+    position_x=linspace(0,L,N+1)'
+
+
+    
+    %plots position
+    figure('Name','2D Comparison')
+    plot(position_x,slices(:, 1),'LineWidth', 2);
+    hold on
+    for i=2:1:num_slices*2
+        plot(position_x, slices(:, i), 'LineWidth', 2);
+    end
+    hold off
+    grid
+%     legendText =  "F: " + string(frontStiffnessDisplay)+ ", R: " + string(rearStiffnessDisplay) + " (kN/rad)";
+    %legend(legendText)
+    xlabel('Position X')
+    ylabel('Temp')
+
+    
+    
+else
+    disp("Unstable")
 end
-
-
-%% plot
-
-figure('Name','Analytical')
-[X, Time] = meshgrid(0:dt:T, x);
-surf(X,Time, exact)
-shading interp
-colormap('jet')
-xlabel('t'); 
-ylabel('x'); 
-zlabel('T(x,t)'); colorbar
-
-figure(2)
-[Time, X] = meshgrid(0:dt:T, x); 
-surf(Time, X, temp); 
-shading interp
-colormap('jet')
-xlabel('t'); 
-ylabel('x'); 
-zlabel('T(x,t)'); colorbar
-
-
-
-
+    
 function exactTemp = exactTemp (x, t, Cn)
     sum = 0;
     
@@ -94,3 +126,5 @@ function exactTemp = exactTemp (x, t, Cn)
     end
     exactTemp = sum + (2*x);
 end
+
+  
